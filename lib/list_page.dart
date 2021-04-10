fd8dc711e7323a4b3adcca7887000ad6eaebffa2
Satriya <satriya.wicaksana888@gmail.com>
2021-04-10 20:14:26 +0700

    adding platform widget to build based on platform

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sandbox/article.dart';
 import 'package:flutter_sandbox/detail_page.dart';
+import 'package:flutter_sandbox/widgets/platform_widget.dart';
 
 
 class NewsListPage extends StatelessWidget {
@@ -8,42 +10,66 @@ class NewsListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    );
+  }
+
+  Widget _buildAndroid(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('News App'),
       ),
-      body: FutureBuilder(
-        future:
-        DefaultAssetBundle.of(context).loadString('assets/articles.json'),
-        builder: (context, snapshot) {
-          final List<Article> articles = parseArticles(snapshot.data);
-          return ListView.builder(
-              itemCount: articles.length,
-              itemBuilder: (context, index) {
-                return _buildArticleItem(context, articles[index]);
-              });
+      body: _buildList(context),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('News App'),
+      ),
+      child: _buildList(context),
+    );
+  }
+
+  Widget _buildList(BuildContext context) {
+    return FutureBuilder(
+      future:
+      DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+      builder: (context, snapshot) {
+        final List<Article> articles = parseArticles(snapshot.data);
+        return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return _buildArticleItem(context, articles[index]);
+            });
+      },
+    );
+  }
+
+  Widget _buildArticleItem(BuildContext context, Article article) {
+    return Material(
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        leading: Image.network(
+          article.urlToImage,
+          width: 100,
+        ),
+        title: Text(article.title),
+        subtitle: Text(article.author),
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            ArticleDetailPage.routeName,
+            arguments: article,
+          );
         },
       ),
     );
   }
 }
 
-Widget _buildArticleItem(BuildContext context, Article article) {
-  return ListTile(
-    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-    leading: Image.network(
-      article.urlToImage,
-      width: 100,
-    ),
-    title: Text(article.title),
-    subtitle: Text(article.author),
-    onTap: () {
-      Navigator.pushNamed(
-        context,
-        ArticleDetailPage.routeName,
-        arguments: article,
-      );
-    },
-  );
-}
+
 
